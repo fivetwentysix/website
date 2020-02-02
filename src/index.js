fe43b65121d2ee82433891bb086b4bfae4a51ef0
Patrick Ma <fivetwentysix@gmail.com>
2020-02-03 07:30:52 +0800

    fix js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import _ from 'lodash';
 import './style.css';
-
-function component() {
-    const element = document.createElement('div');
-    element.classList = ['footer'];
-    element.innerHTML = _.join(['Handcrafted. Source avai'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
