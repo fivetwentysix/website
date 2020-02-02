5df6a9566d56b72eb9cbd325a73c113da9adaf35
Patrick Ma <fivetwentysix@gmail.com>
2020-02-03 06:18:44 +0800

    improve webpack config

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
     const element = document.createElement('div');
 
     // Lodash, curren   tly included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack!!!'], ' ');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,5 +7,15 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: "development"
+    mode: "development",
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: "./dist"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: 'src/index.html'
+        }),
+    ],
 };
